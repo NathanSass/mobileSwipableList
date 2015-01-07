8c0bc1bc7209201e86c5c3cfb3de01709c0308c7
Nathan Sass <Nathan.Sass@chase.com>
2015-01-07 10:30:22 -0800

    targeting the mousemove of an element so I can add padding and show dragging

--- js/app.js
+++ js/app.js
@@ -1,6 +1,32 @@
 $(function() {
-    $(".message-group li").on("swipe", function(e){
-    	$(e.target).addClass("hidden");
-    	$(e.target).next().removeClass("hidden").addClass("show");
+    // $(".message-group li").on("touchmove", function(e){
+    // 	debugger
+    // 	// $(e.target).closest('li').on("vmousedown", function(event){
+    // 	// 	debugger
+    // 	// })
+    // 	// console.log(e.swipestop.coords[0] - e.swipestart.coords[0])
+    // 	// $(e.target).addClass("hidden");
+    // 	// $(e.target).next().removeClass("hidden").addClass("show");
+    // })
+
+  //   $("li").on("touchstart",function(e){
+  //     debugger
+  //     e.preventDefault();
+		// });
+
+
+		$(".message-group li")
+    .on('touchstart mousedown', function(e){
+    	e.preventDefault();
+    	// console.log("hkjh")
+    	// debugger
+    	$(e.target).closest('li')
+	    .on('touchmove mousemove', function(e){
+	    	e.preventDefault();
+	    	// debugger
+	    	$(e.target).closest('li span').css("color","red")
+	    })
+
     })
+    // .on('touchend touchcancel', function(e){debugger})
 });
