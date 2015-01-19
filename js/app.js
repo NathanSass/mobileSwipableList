83de6847498dffef0ddc0f3137b55a8a45b1ba27
Nathan Sass <nathansass@Nathans-MacBook-Air.local>
2015-01-19 12:50:06 -0800

    have ractive decorator up and running

--- js/app.js
+++ js/app.js
@@ -1,46 +1,58 @@
 $(function() {
-    // $(".message-group li").on("touchmove", function(e){
-    // 	debugger
-    // 	// $(e.target).closest('li').on("vmousedown", function(event){
-    // 	// 	debugger
-    // 	// })
-    // 	// console.log(e.swipestop.coords[0] - e.swipestart.coords[0])
-    // 	// $(e.target).addClass("hidden");
-    // 	// $(e.target).next().removeClass("hidden").addClass("show");
-    // })
+  var swipeableDecorator= function(node,content) {
+    alert("HIII");
+    console.log("sdfdsfsdfs");
+    return {
+      teardown: function(){
 
-  //   $("li").on("touchstart",function(e){
-  //     debugger
-  //     e.preventDefault();
-		// });
-	// var startX,
-	// 	startY;
+      }
+    }
+  }
 
-	function getCoord (e, c) {
-		return /touch/.test(e.type) ? (e.originalEvent || e).changedTouches[0]['page' + c] : e['page' + c];
-	}
+  Ractive.decorators.swipeable = swipeableDecorator;
 
-	$(".message-group li")
-  .on('touchstart mousedown', function(ev){
-		startX = getCoord(ev, 'X');
-		startY = getCoord(ev, 'Y');
+var ractive = new Ractive({
+  // The `el` option can be a node, an ID, or a CSS selector.
+  el: 'container',
+
+  // We could pass in a string, but for the sake of convenience
+  // we're passing the ID of the <script> tag above.
+  template: '#template'
+});
+
+// var ractive = new Ractive({
+//     el: 'body',
+//     template: #template,
+//     decorators: {
+//       swipeable: swipeableDecorator
+//     }
+//   });
+
+ //  function getCoord (e, c) {
+ //    return /touch/.test(e.type) ? (e.originalEvent || e).changedTouches[0]['page' + c] : e['page' + c];
+ //  }
+
+	// $(".message-group li")
+ //  .on('touchstart mousedown', function(ev){
+	// 	startX = getCoord(ev, 'X');
+	// 	startY = getCoord(ev, 'Y');
 
 
-  	$(ev.target).closest('li').on('touchmove mousemove', function(e){
-    	e.preventDefault();
-    	currentX = getCoord(e, 'X');
-    	currentY = getCoord(e, 'Y');
-    	if(currentX - startX < 0){ //Do something here to account for vertical swiping
-    		console.log("swipingleft");
-    		// debugger
-    		var distance =  startX - currentX;
-    		// console.log(dist)
-    		$(e.target).css("-webkit-transform", "translateX(" + -distance + "px)")
-    		// $(e.target).css("-webkit-transform", "translateX(" + startX - currentDistX + "px)")
-    	}
+ //  	$(ev.target).closest('li').on('touchmove mousemove', function(e){
+ //    	e.preventDefault();
+ //    	currentX = getCoord(e, 'X');
+ //    	currentY = getCoord(e, 'Y');
+ //    	if(currentX - startX < 0){ //Do something here to account for vertical swiping
+ //    		console.log("swipingleft");
+ //    		// debugger
+ //    		var distance =  startX - currentX;
+ //    		// console.log(dist)
+ //    		$(e.target).css("-webkit-transform", "translateX(" + -distance + "px)")
+ //    		// $(e.target).css("-webkit-transform", "translateX(" + startX - currentDistX + "px)")
+ //    	}
 
-    })
+ //    })
 
-  })
+ //  })
   // .on('touchend touchcancel', function(e){debugger})
 });
